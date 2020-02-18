0937be083fb236876a9680955d384dce46eb048a
anil vaja <anilvaja.007@gmail.com>
2020-02-19 01:45:11 +0530

    Laravel Project Setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->Integer('user_type');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+//            $table->foreign('user_type')->references('id')->on('user_types');
         });
     }
 
